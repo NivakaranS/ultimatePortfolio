bd7e3fe8e2aa8c090e7604949444d54dcfe949a1
NivakaranS <nivakaran@hotmail.com>
2024-07-08 10:33:37 +0530

    Another commit message

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,19 +9,30 @@ const Footer = () => {
         <div className='mainFooterContainer'>
             <div >
                 <div style={{textAlign: 'center', color: 'yellow', fontSize: '20px', marginBottom: '10px'}}>
-                    Contact
+                    Let's connect
                 </div>
                 
                 <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <img width="24" height="24" src="https://img.icons8.com/material-rounded/24/mail.png" alt="mail"/>
+                <img width="24" height="24" style={{marginRight: '5px'}} src="https://img.icons8.com/material-rounded/24/mail.png" alt="mail"/>
 
                     nivakaran@hotmail.com
                 </div>
             </div>
             <div style={{margin: '4px 0px'}}>
+                <div style={{display: 'flex', flexDirection: 'row'}}>
+                <div className='contactOptions'>
                 <a href='https://www.linkedin.com/in/nivakaran/'><img src={LinkedIn} style={{height: '25px', cursor: 'pointer', margin: '0px 8px'}}/></a>
+                </div>
+                <div className='contactOptions'>
                 <a href='https://github.com/NivakaranS'><img src={Git} style={{height: '25px', cursor: 'pointer', margin: '0px 8px'}}/></a>
+                </div>
+                <div className='contactOptions'>
                 <a href='https://www.instagram.com/nivakarans/'><img src={Instagram} style={{height: '25px', cursor: 'pointer', margin: '0px 8px'}}/></a>
+                </div>
+
+                </div>
+                
+                
             </div>
             <hr style={{width: '95%'}}/>
             <div style={{marginTop: '5px', textAlign: 'center'}}>
